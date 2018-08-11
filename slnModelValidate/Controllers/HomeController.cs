a8bc1f310b7e76b717f5ac2663efee00589c8963
kiziko76121 <kiziko76121@gmail.com>
2018-08-11 17:53:25 +0800

    第四次提交

--- slnModelValidate/Controllers/HomeController.cs
+++ slnModelValidate/Controllers/HomeController.cs
@@ -3,17 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using slnModelValidate.Models;
 
 namespace slnModelValidate.Controllers
 {
     public class HomeController : Controller
     {
+        dbStudentEntities db = new dbStudentEntities();
+
         public ActionResult Index()
+        {
+            var standents = db.tStudent.ToList();
+            return View(standents);
+        }
+
+        public ActionResult Create()
         {
             return View();
         }
 
-        public ActionResult About()
+        [HttpPost]
+        public ActionResult Create(tStudent student)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tStudent.Add(student);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+
+        public ActionResult Delete(String id)
+        {
+            var student = db.tStudent.
+                Where(m=>m.fStuId==id).FirstOrDefault();
+            db.tStudent.Remove(student);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+            public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
